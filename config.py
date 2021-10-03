109bb3e7a8f547114cf44ea64eae37e7f70f4f4e
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-10-03 11:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
-    BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
+    BUTTON_TEXT = os.environ.get("BUTTON", "")
     URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
