901540f7367c5d8c3e38242bc13d7ef02dca4bdf
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-10-03 15:34:27 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -17,8 +17,6 @@ URL_LINK=Config.URL_LINK
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
     await message.edit(f"<b>{kopp.file_name}</b>\n\n<b>{CAPTION_TEXT}</b>",
-          reply_markup=InlineKeyboardMarkup(
-        ))
 
 def get_file_id(msg: Message):
     if msg.media:
