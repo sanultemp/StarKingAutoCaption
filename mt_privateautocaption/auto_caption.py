dc81b0d41ecd2819ba49d955553ca1e2050746de
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-10-04 12:00:38 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K & PR0FESS0R-99
+# (c) StarKing Developers
 
 import os
 from config import Config
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
@@ -16,7 +16,7 @@ URL_LINK=Config.URL_LINK
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<code>{kopp.file_name}</code>\n\n<b>{CAPTION_TEXT}</b>")
+    await message.edit(f"Name: <code>{kopp.file_name}</code>\n\n<b>{CAPTION_TEXT}</b>")
 
 def get_file_id(msg: Message):
     if msg.media:
