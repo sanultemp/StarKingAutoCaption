e6ed359c28f72fa29c76b2196af05e0616689765
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-10-15 10:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-# <C> MoTechYT
+# <C> StarKing Developers
 
 
 import os
 
 class Config(object):
-    MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
-    BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
-    URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "2043641521:AAF7DRIsY-bNc_rfPWED1w_LkWZ7CmVSXUM")
+    API_ID = int(os.environ.get("APP_ID", 7469109))
+    API_HASH = os.environ.get("API_HASH", "4d4023337b8cc46c306af69adb5fc21a")
+    CAPTION = os.environ.get("CAPTION", "𝗙𝗟𝗜𝗫 𝗖𝗜𝗡𝗘𝗠𝗔 ™°\n@Flix_CinemaSL")
+    BUTTON_TEXT = os.environ.get("BUTTON", "")
+    URL_LINK = os.environ.get("LINK", "https://t.me/flixcimaslk")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@FlixCaption_Bot")
